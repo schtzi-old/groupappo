786118b2b81dbbb585e1bf68db8abdad6e5314b8
ryan boyd <ryantylerboyd@outlook.com>
2022-11-28 16:34:13 +1100

    removed line

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -1,5 +1,4 @@
 class Participation < ApplicationRecord
   belongs_to :gruppetto
   belongs_to :user
-  enum :booking_status, { draft: 0, unconfirmed: 1, confirmed: 2, completed: 3, declined: 4 }, default: :draft
 end
