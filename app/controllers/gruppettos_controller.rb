ce96794b79d4b06e5ae288f1bf25313d37afa4fb
ryan boyd <ryantylerboyd@outlook.com>
2022-11-29 12:40:05 +1100

    updated the policy for gruppetto index

--- app/controllers/gruppettos_controller.rb
+++ app/controllers/gruppettos_controller.rb
@@ -1,5 +1,14 @@
 class GruppettosController < ApplicationController
   def index
+    @gruppettos = policy_scope(Gruppetto)
+    # The `geocoded` scope filters only flats with coordinates
+    # For each Gruppetto get the latitiude and the longitude. Then save it in an array of markers.
+    # @markers = @gruppettos.geocoded.map do |gruppetto|
+    #   {
+    #     lat: gruppetto.track.latitude,
+    #     lng: gruppetto.track.longitude
+    #   }
+    # end
   end
 
   def show
--- app/policies/gruppetto_policy.rb
+++ app/policies/gruppetto_policy.rb
@@ -1,8 +1,12 @@
 class GruppettoPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      scope.all
+    end
+  end
+
+  def index?
+    true
   end
 end
