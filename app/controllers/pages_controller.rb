ea441847de3578437a3248e5a809e17b3ab81190
Shelley Timmins <104867321+timminsss@users.noreply.github.com>
2022-11-29 12:23:18 +1100

    Update pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, except: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
   end
