7d849dd18737d94afa54231669cb0455b00185ac
Jan Schuetz <janalexschuetz@gmail.com>
2022-11-30 15:01:16 +1100

    added relevant stuff for sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   get 'gruppettos/create'
   devise_for :users
   root to: "pages#home"
+
+  require "sidekiq/web"
+  authenticate :user, ->(user) { user.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
