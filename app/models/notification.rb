41928fec4b456b9f768d01a194b6e7645f6cebbd
Ryan Boyd <114888159+ryantylerboyd@users.noreply.github.com>
2022-12-07 16:28:10 +1100

    Update notification.rb

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,24 +2,5 @@ class Notification < ApplicationRecord
   include Noticed::Model
   belongs_to :recipient, polymorphic: true
 
-  after_create_commit :broadcast_to_recipient
 
-  def broadcast_to_recipient
-    # broadcast_append_later_to(
-    #   recipient,
-    #   :notifications,
-    #   # target: 'notification-count',
-    #   # partial: 'notifications/notification',
-    #   count: Notification.count,
-    #   locals: {
-    #     notification: self
-    #   }
-    # )
-
-    #NotificationsChannel.broadcast_to(
-     # self,
-     # {count: Notification.count}
-    #)
-    raise
-  end
 end
